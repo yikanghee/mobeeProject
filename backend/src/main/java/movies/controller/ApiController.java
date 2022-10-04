c139c7f1d8e8e9b5e408dbc3fc4eec4c99ab2b73
yikanghee <yikanghee2@naver.com>
2022-10-04 14:21:32 +0900

    [#4] feature : 크롤링 서비스

--- backend/src/main/java/movies/controller/ApiController.java
+++ backend/src/main/java/movies/controller/ApiController.java
@@ -4,9 +4,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import movies.service.CrowlingService;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -25,8 +21,6 @@ import java.net.URL;
 public class ApiController {
 
     private final movies.service.ApiService ApiService;
-    private final CrowlingService crowlingService;
-
 
     private final String KEY = "bf4027c3100b9e4e2dc3221cfb994433";
 
