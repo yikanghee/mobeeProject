99bce545b873353aafdca0614fca9935e12a1bd1
yikanghee <yikanghee2@naver.com>
2022-10-07 18:16:02 +0900

    [#6] feature : 크롤링 영화 데이터 영어 변역

--- backend/src/main/java/movies/config/constants/ApiUrlConstants.java
+++ backend/src/main/java/movies/config/constants/ApiUrlConstants.java
@@ -7,4 +7,7 @@ import lombok.NoArgsConstructor;
 public class ApiUrlConstants {
     public static final String MOVIE_URL = "https://movie.naver.com/movie/sdb/rank/rmovie.naver";
     public static final String URL = "https://movie.naver.com/movie/bi/mi/basic.naver?code=";
+
+    public static final String TRANCELATEURL = "https://openapi.naver.com/v1/papago/n2mt";
+
 }
--- backend/src/main/java/movies/service/impl/CrowlingServiceImpl.java
+++ backend/src/main/java/movies/service/impl/CrowlingServiceImpl.java
@@ -1,7 +1,9 @@
 package movies.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import movies.Utils.NetworkUtil;
 import movies.config.constants.ApiUrlConstants;
 import movies.domain.CrowlingMovie;
 import movies.repository.CrowlingRepository;
@@ -10,20 +12,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URLEncoder;
+import java.util.*;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CrowlingServiceImpl implements CrowlingService {
 
+    @Value("${naver.papago.clientId}")
+    private String clientId;
+
+    @Value("${naver.papago.clientSecret}")
+    private String clientSecret;
+
     private String codeUrl = ApiUrlConstants.MOVIE_URL;
     private String Url = ApiUrlConstants.URL;
     private final CrowlingRepository crowlingRepository;
+
+    private String translateApiURL = ApiUrlConstants.TRANCELATEURL;
 
     public void getCrowling() throws IOException {
 
@@ -83,5 +94,51 @@ public class CrowlingServiceImpl implements CrowlingService {
     public void deleteCrowling() {
         crowlingRepository.deleteAll();
     }
+
+    public String translate(String contents) throws Exception {
+
+        log.info(this.getClass().getName() + "traslate Start!");
+
+        String source = "ko";
+        String target = "en";
+
+        String postParams = "source=" + source + "&target=" + target + "&text=" + URLEncoder.encode(contents, "UTF-8");
+
+        log.info("postParams : " + postParams);
+
+        String json = NetworkUtil.post(translateApiURL, this.setNaverInfo(), postParams);
+
+        log.info("json : " + json);
+
+        Map<String, Object> rMap = new ObjectMapper().readValue(json, LinkedHashMap.class);
+
+        // 결과 내용 중 message 정보가져오기
+        Map<String, Object> messageMap = (Map<String, Object>) rMap.get("message");
+
+        // message 결과 내용 중 result 정보가져오기
+        Map<String, String> resultMap = (Map<String, String>) messageMap.get("result");
+
+        log.info("resultMap : " + resultMap);
+
+        String translatedText = resultMap.get("translatedText");
+
+        return translatedText;
+    }
+
+    private Map<String, String> setNaverInfo() {
+        log.info("### setNaverInfo start");
+        HashMap<String, String> requestheader = new HashMap<>();
+
+        requestheader.put("X-Naver-Client-Id", clientId);
+        requestheader.put("X-Naver-Client-Secret", clientSecret);
+
+        log.info("### clientId : {}", clientId);
+        log.info("### clientSecret : {}", clientSecret);
+
+        log.info("### setNaverInfo end");
+        return requestheader;
+    }
+
+
 
 }
